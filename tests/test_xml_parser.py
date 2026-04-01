ec6871e1f3ae25c02e3a0a5504633a1526794da8
Jonas Arndt <joebubi@posteo.de>
2026-04-01 14:10:27 +0200

    added red+green for FileXmlLoader class

--- tests/test_xml_parser.py
+++ tests/test_xml_parser.py
@@ -1,4 +1,4 @@
-from src.modules.xml_parser import CommandElement, TreeElement, XmlLoader
+from src.modules.xml_parser import CommandElement, TreeElement, XmlLoader, FileXmlLoader
 import xml.etree.ElementTree as ET
 import pytest
 from pydantic import ValidationError
@@ -38,7 +38,7 @@ def xml_element(request):
 
 expected_data = [(data, data) for data in test_data_good]
 @pytest.mark.parametrize("xml_element, expected_data", expected_data, indirect=["xml_element"])
-def test_class_CommandElement_good_path(xml_element: ET.Element, expected_data):
+def test_class_CommandElement_good(xml_element: ET.Element, expected_data):
     cut = CommandElement.fill_from_xml(xml_element)
     assert(cut.name == expected_data['name'])
     assert(cut.unittype == expected_data['unittype'])
@@ -47,7 +47,7 @@ def test_class_CommandElement_good_path(xml_element: ET.Element, expected_data):
 
 expected_data = [(data, data) for data in test_data_bad]
 @pytest.mark.parametrize("xml_element, expected_data", expected_data, indirect=["xml_element"])
-def test_class_CommandElement_bad_path(xml_element: ET.Element, expected_data):
+def test_class_CommandElement_bad(xml_element: ET.Element, expected_data):
     with (pytest.raises(ValidationError) as exc):
        CommandElement.fill_from_xml(xml_element)
     assert exc.type is ValidationError
@@ -72,7 +72,7 @@ test_xml_string_good = [
 
 expected_data = [(data['expected_data'],data) for data in test_xml_string_good]
 @pytest.mark.parametrize("expected_data, fake_loader", expected_data, indirect=["fake_loader"])
-def test_class_TreeElement_fetch_data_good_path(expected_data, fake_loader: FakeXmlLoader):
+def test_class_TreeElement_fetch_data_good(expected_data, fake_loader: FakeXmlLoader):
     path = ""
     cut = TreeElement.fetch_data(path, fake_loader)
     for i, item in enumerate(cut.cmd_list):
@@ -81,15 +81,49 @@ def test_class_TreeElement_fetch_data_good_path(expected_data, fake_loader: Fake
         assert item.desc == expected_data[i]['desc']
 
 
+
+test_xml_string_devices_bad = [
+    {"xml_string": ""},
+    {"xml_string": "<?xml version=\"1.0\"?>"},
+    {"xml_string": "<?xml version=\"1.0\"?><vito><comman"},
+]
+
+@pytest.fixture
+def path_loader(request, tmp_path):
+    data = request.param
+    filename = str(tmp_path / "test.xml")
+    with open(filename, "w") as f:
+        f.write(data['xml_string'])
+    
+    return filename
+
+@pytest.mark.parametrize("path_loader", test_xml_string_devices_bad, indirect=["path_loader"])
+def test_class_FileXmlLoader_bad_content(path_loader):
+    with (pytest.raises(ET.ParseError) as exc):    
+        cut = FileXmlLoader().load(path_loader)
+    assert exc.type is ET.ParseError
+
+def test_class_FileXmlLoader_bad_path():
+    with (pytest.raises(FileNotFoundError) as exc):
+        cut = FileXmlLoader().load("")
+    assert exc.type is FileNotFoundError
+
+
 test_xml_string_devices_good = [
     {"xml_string": "<?xml version=\"1.0\"?><vito><devices><device ID=\"2098\"/><device ID=\"2053\"/></devices><commands><command name=\"getTempA\"><addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description><device ID=\"2053\"><addr>6F</addr><unit>UT1</unit><len>1</len></device></command><command name=\"getTempAtp\"><addr>5525</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C (Tiefpass)</description><device ID=\"2053\"/></command></commands></vito>",
+     "expected_data": "<vito><commands><command name=\"getTempA\"><addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description></command><command name=\"getTempAtp\"><addr>5525</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C (Tiefpass)</description></command></commands></vito>"},
+    {"xml_string": "<?xml version=\"1.0\"?><vito><commands><command name=\"getTempA\"><addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description><device ID=\"2053\"><addr>6F</addr><unit>UT1</unit><len>1</len></device></command><command name=\"getTempAtp\"><addr>5525</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C (Tiefpass)</description><device ID=\"2053\"/></command></commands></vito>",
+     "expected_data": "<vito><commands><command name=\"getTempA\"><addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description></command><command name=\"getTempAtp\"><addr>5525</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C (Tiefpass)</description></command></commands></vito>"},
+    {"xml_string": "<?xml version=\"1.0\"?><vito><commands><command name=\"getTempA\"><addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description></command><command name=\"getTempAtp\"><addr>5525</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C (Tiefpass)</description></command></commands></vito>",
      "expected_data": "<vito><commands><command name=\"getTempA\"><addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description></command><command name=\"getTempAtp\"><addr>5525</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C (Tiefpass)</description></command></commands></vito>"},
 ]
 
 expected_data = [(data['expected_data'], data) for data in test_xml_string_devices_good]
 @pytest.mark.parametrize("expected_string, fake_loader", expected_data, indirect=["fake_loader"])
-def test_class_TreeElement_remove_device_references(expected_string: str, fake_loader: FakeXmlLoader):
+def test_class_TreeElement_remove_device_references_good(expected_string: str, fake_loader: FakeXmlLoader):
     path = ""
     cut = TreeElement.fetch_data(path, fake_loader)
     cut.remove_dev_refs()
     assert ET.tostring(cut.root_elem, encoding='unicode', method='xml') == expected_string
+
+
