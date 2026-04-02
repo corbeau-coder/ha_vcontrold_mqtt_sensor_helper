6bc6f32d139dccf476b0e38a87e4d7a1d18b21e0
Jonas Arndt <joebubi@posteo.de>
2026-04-02 11:16:04 +0200

    starting red phase for int test yaml generation

---
+++ tests/template_engine/test_template_engine.py
@@ -0,0 +1,10 @@
+
+
+test_xml_string_template_good = {
+    "xml_string": "<?xml version=\"1.0\"?><vito><devices><device ID=\"2098\"/><device ID=\"2053\"/></devices><commands><command name=\"TestsensorTemp1\"> \
+                    <addr>0800</addr><len>2</len><unit>UT</unit><description>Ermittle die Aussentemperatur in Grad C</description><device ID=\"2053\"> \
+                    <addr>6F</addr><unit>UT1</unit><len>1</len></device></command></commands></vito>",
+    "sample_file_name": "sample_files/sensor.yaml"}
+
+def test_yaml_samples:
+    #mocke input datei mit xml_string, asserte gegen geparsten Teil von sample_file_name
